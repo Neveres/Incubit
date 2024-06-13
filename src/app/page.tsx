868f896fe494011c18400777d17fb1cf15f7a43a
Leo Wu <leo90068@yahoo.com.tw>
2024-06-13 21:47:21 +0800

    feat: replace with GraphViewer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,40 @@
-import { Box } from "@/components";
+"use client";
+import { useEffect, useState } from "react";
+import { GraphViewer } from "@/components";
 
 export default function Home() {
+  const [graph, setGraph] = useState<Incubit.Graph>({ nodes: [], edges: [] });
+
+  const handleEdgeLabelChange = (from: number, to: number, label: string) => {
+    setGraph((prevGraph) => ({
+      ...prevGraph,
+      edges: prevGraph.edges.map((edge) =>
+        edge.source === from && edge.target === to
+          ? { ...edge, value: label }
+          : edge
+      ),
+    }));
+  };
+
+  useEffect(() => {
+    fetch("/graph.json")
+      .then((response) => response.json())
+      .then((data) => {
+        const nodesWithPositions = data.nodes.map((node: any) => ({
+          ...node,
+          position: [
+            Math.random() * 10 - 5,
+            Math.random() * 10 - 5,
+            Math.random() * 10 - 5,
+          ],
+        }));
+        setGraph({ ...data, nodes: nodesWithPositions });
+      });
+  }, []);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Box />
+      <GraphViewer graph={graph} onEdgeLabelChange={handleEdgeLabelChange} />
     </main>
   );
 }
