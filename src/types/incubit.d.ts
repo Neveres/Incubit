0c97f52ec66e46e32abf43061f601f25e2c1bc93
Leo Wu <leo90068@yahoo.com.tw>
2024-06-13 21:46:36 +0800

    feat: add types

---
+++ src/types/incubit.d.ts
@@ -0,0 +1,20 @@
+declare namespace Incubit {
+  interface Node {
+    id: number;
+    name: string;
+    path: string;
+    position?: [number, number, number];
+  }
+
+  interface Edge {
+    id: number;
+    source: number;
+    target: number;
+    value: string;
+  }
+
+  interface Graph {
+    nodes: Node[];
+    edges: Edge[];
+  }
+}
