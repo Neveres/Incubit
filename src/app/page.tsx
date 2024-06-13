824793c8893e0976ecb6b8d4c0728a798752dada
Leo Wu <leo90068@yahoo.com.tw>
2024-06-13 22:19:43 +0800

    feat: rm padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,7 +33,7 @@ export default function Home() {
   }, []);
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between">
       <GraphViewer graph={graph} onEdgeLabelChange={handleEdgeLabelChange} />
     </main>
   );
